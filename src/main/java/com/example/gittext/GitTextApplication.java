a86c96620f0b48496e75bdb301a10489e176d657
mayuxiang <mayuxiang0383@foxmail.com>
2020-08-17 14:18:59 +0800

    没什么变化

--- src/main/java/com/example/gittext/GitTextApplication.java
+++ src/main/java/com/example/gittext/GitTextApplication.java
@@ -1,7 +1,7 @@
 package com.example.gittext;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitTextApplication {
